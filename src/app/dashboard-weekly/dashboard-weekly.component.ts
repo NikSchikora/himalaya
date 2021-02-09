16ef22f125c0cab9bdc38d45a721272a6d5b912a
Niklas Schikora <info@niklas-schikora.de>
2021-02-09 13:54:57 +0100

    Small layout changes and implementations of name and date vaiables

--- src/app/dashboard-weekly/dashboard-weekly.component.ts
+++ src/app/dashboard-weekly/dashboard-weekly.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-weekly',
   templateUrl: './dashboard-weekly.component.html',
-  styleUrls: ['./dashboard-weekly.component.css']
+  styleUrls: ['./dashboard-weekly.component.css'],
 })
 export class DashboardWeeklyComponent implements OnInit {
+  public name: string = 'Max Mustermann';
+  public date: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
