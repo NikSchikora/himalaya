5c3afd52ab9ed4713cf08d8bf31063c2bd1c1762
Niklas Schikora <info@niklas-schikora.de>
2021-03-03 09:49:30 +0100

    calculation fix for total time and added mat-card for container

--- src/app/components/total-timetracking/total-timetracking.component.ts
+++ src/app/components/total-timetracking/total-timetracking.component.ts
@@ -1,4 +1,4 @@
-import { TimeTrackingService } from './../../services/time-tracking.service';
+import { DataService } from 'src/app/services/data-service.service';
 import { Component, OnInit } from '@angular/core';
 import { TimeTracking } from 'src/app/models/time-tracking';
 
@@ -9,19 +9,22 @@ import { TimeTracking } from 'src/app/models/time-tracking';
 })
 export class TotalTimetrackingComponent implements OnInit {
   totalTime: number = 0;
+  totalTimeMinutes: number = 0;
 
-  constructor(private timeTrackingService: TimeTrackingService) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.calculateTotalTime();
   }
 
   async calculateTotalTime() {
-    let times: TimeTracking[] = await this.timeTrackingService.getAll();
-
-    // times.forEach((tracking: TimeTracking) => {
-    //   this.totalTime +=
-    //     tracking.endDate.getTime() - tracking.startDate.getTime();
-    // });
+    this.dataService.timeTrackings.forEach((tracking: TimeTracking) => {
+      this.totalTime += tracking.duration.hours;
+      this.totalTimeMinutes += tracking.duration.minutes;
+      if (this.totalTimeMinutes > 59) {
+        this.totalTime++;
+        this.totalTimeMinutes = 0;
+      }
+    });
   }
 }
